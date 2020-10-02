dd41d96fca10b090c696628c92fb74fc9209cfc4
Andrea F. Daniele <afdaniele@ttic.edu>
2020-10-02 16:00:37 -0500

    moved `daffy` to the end of the library name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 # The package name is the name on PyPI
 # it is not the python module names.
 distro = 'daffy'
-package_name = 'dt-{}-authentication'.format(distro)
+package_name = 'dt-authentication-{}'.format(distro)
 library_webpage = 'http://github.com/duckietown/lib-dt-authentication'
 maintainer = 'Andrea F. Daniele'
 maintainer_email = 'afdaniele@ttic.edu'

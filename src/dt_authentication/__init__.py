4979e143b6a7a63f1bea7e5a7ac0fdf6eec5af8e
Andrea Censi <acensi@ethz.ch>
2021-10-21 13:23:44 +0200

    update of pre-commit

--- src/dt_authentication/__init__.py
+++ src/dt_authentication/__init__.py
@@ -4,7 +4,4 @@ from .exceptions import InvalidToken
 from .token import DuckietownToken
 
 
-__all__ = [
-    'DuckietownToken',
-    'InvalidToken'
-]
+__all__ = ["DuckietownToken", "InvalidToken"]

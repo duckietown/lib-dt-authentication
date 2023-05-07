bc7464b5bc059a5dbcc1c2bcce9e21d401c26de9
Andrea F. Daniele <afdaniele@duckietown.com>
2023-05-07 03:24:16 -0400

    added support for tokens dt2

--- src/dt_authentication/token.py
+++ src/dt_authentication/token.py
@@ -1,20 +1,41 @@
 import copy
-import json
 import datetime
-from typing import Dict, Union
+import json
+from typing import Dict, Union, List, Optional
 
 from base58 import b58decode, b58encode
-from ecdsa.keys import VerifyingKey, BadSignatureError
+# noinspection PyProtectedMember
+from ecdsa import NIST192p
+from ecdsa.keys import VerifyingKey, BadSignatureError, SigningKey
 
 from .exceptions import InvalidToken
+from .scope import Scope
 
-PUBLIC_KEY = \
-    """-----BEGIN PUBLIC KEY-----
+PUBLIC_KEYS = {
+    # dt1 was introduced in 2017
+    "dt1": """-----BEGIN PUBLIC KEY-----
 MEkwEwYHKoZIzj0CAQYIKoZIzj0DAQEDMgAEQr/8RJmJZT+Bh1YMb1aqc2ao5teE
 ixOeCMGTO79Dbvw5dGmHJLYyNPwnKkWayyJS
+-----END PUBLIC KEY-----""",
+    # dt2 was introduced in May 2023
+    "dt2": """-----BEGIN PUBLIC KEY-----
+MEkwEwYHKoZIzj0CAQYIKoZIzj0DAQEDMgAEHIqMBPGB2tzRgrMKhQSkEiKQ317q
+msEAqq1CS86oV1vjHYVq6FLvtnDsuWzbW2Nz
 -----END PUBLIC KEY-----"""
+}
+DATETIME_FORMAT = {
+    "dt1": "%Y-%m-%d",
+    "dt2": "%Y-%m-%d/%H:%M"
+}
 
-PAYLOAD_FIELDS = {'uid', 'exp'}
+PAYLOAD_FIELDS = {"uid", "exp"}
+CURVE = NIST192p
+SUPPORTED_VERSIONS = ["dt1", "dt2"]
+DEFAULT_VERSION = "dt2"
+
+
+ScopeList = List[Union[Scope, str]]
+DEFAULT_SCOPE = [Scope(action="auth")]
 
 
 class DuckietownToken(object):
@@ -25,9 +46,8 @@ class DuckietownToken(object):
         payload:    The token's payload as a dictionary.
         signature:  The token's signature.
     """
-    VERSION = 'dt1'
 
-    def __init__(self, payload: Dict[str, Union[str, int]], signature: Union[str, bytes]):
+    def __init__(self, version: str, payload: Dict[str, Union[str, int]], signature: Union[str, bytes]):
         """
         Creates a Duckietown Token from a payload and a signature.
 
@@ -35,11 +55,20 @@ class DuckietownToken(object):
         :py:method:`dt_authentication.DuckietownToken.from_string` instead of instantiating this
         class directly.
 
+        :param version:     A string indicating the version of the token
         :param payload:     A dictionary containing the token payload
         :param signature:   A signature, either as a base58 encoded string or as raw bytes
         """
-        self._payload: Dict[str, Union[str, int]] = payload
+        self._version: str = version
+        self._payload: Dict[str, Union[str, int, list]] = payload
         self._signature: bytes = signature if isinstance(signature, (bytes,)) else b58decode(signature)
+
+    @property
+    def version(self) -> str:
+        """
+        The version of this token
+        """
+        return self._version
 
     @property
     def payload(self) -> Dict[str, str]:
@@ -63,32 +92,63 @@ class DuckietownToken(object):
         return self._payload['uid']
 
     @property
-    def expiration(self) -> datetime.date:
+    def scope(self) -> List[Scope]:
+        """
+        The scope of this token.
+        """
+        return self._payload.get("scope", DEFAULT_SCOPE)
+
+    @property
+    def expiration(self) -> Optional[datetime.date]:
         """
         The token's expiration date.
         """
-        return datetime.date(*map(int, self._payload['exp'].split('-')))
+        if self._payload['exp'] is None:
+            return None
+        return datetime.datetime.strptime(self._payload['exp'], DATETIME_FORMAT[self._version])
+
+    @property
+    def expired(self) -> bool:
+        """ Whether the token is already expired """
+        exp: Optional[datetime.date] = self.expiration
+        if exp is None:
+            # never-expiring tokens
+            return False
+        # compare now() with token expiration date
+        return exp < datetime.datetime.now()
 
     def as_string(self) -> str:
         """
         Returns the Duckietown Token string.
         """
+        # encode scope
+        scope: List[Union[str, dict]] = [s.compact() for s in self.scope]
         # encode payload into JSON
-        payload_json: str = json.dumps(self._payload)
+        payload = copy.deepcopy(self._payload)
+        payload["scope"] = scope
+        payload_json: str = json.dumps(payload, sort_keys=True)
         # encode payload and signature
         payload_base58: str = b58encode(payload_json).decode("utf-8")
         signature_base58: str = b58encode(self._signature).decode("utf-8")
         # compile token
-        return f"{self.VERSION}-{payload_base58}-{signature_base58}"
+        return f"{self._version}-{payload_base58}-{signature_base58}"
+
+    def grants(self, action: str, resource: Optional[str] = None, identifier: Optional[str] = None,
+               service: Optional[str] = None) -> bool:
+        for s in self.scope:
+            if s.grants(action, resource, identifier, service):
+                return True
+        return False
 
     @staticmethod
-    def from_string(s: str) -> 'DuckietownToken':
+    def from_string(s: str, vk: Optional[VerifyingKey] = None) -> 'DuckietownToken':
         """
         Decodes a Duckietown Token string into an instance of
         :py:class:`dt_authentication.DuckietownToken`.
 
         Args:
-            s:  The Duckietown Token string.
+            s:   The Duckietown Token string.
+            vk:  Optional verification key if different from default
 
         Raises:
             InvalidToken:   The given token is not valid.
@@ -101,13 +161,14 @@ class DuckietownToken(object):
         # unpack components
         version, payload_base58, signature_base58 = p
         # check token version
-        if version != DuckietownToken.VERSION:
+        if version not in SUPPORTED_VERSIONS:
             raise InvalidToken("Duckietown Token version '%s' not supported" % version)
         # decode payload and signature
         payload_json = b58decode(payload_base58)
         signature = b58decode(signature_base58)
         # verify token
-        vk = VerifyingKey.from_pem(PUBLIC_KEY)
+        if not vk:
+            vk = VerifyingKey.from_pem(PUBLIC_KEYS[version])
         is_valid = False
         try:
             is_valid = vk.verify(signature, payload_json)
@@ -121,5 +182,42 @@ class DuckietownToken(object):
         if not isinstance(payload, dict) or \
                 len(set(payload.keys()).intersection(PAYLOAD_FIELDS)) != len(PAYLOAD_FIELDS):
             raise InvalidToken("Duckietown Token has an invalid payload")
+        # parse scope
+        if "scope" in payload:
+            payload["scope"] = [Scope.parse(s) for s in payload["scope"]]
         # ---
-        return DuckietownToken(payload, signature)
+        return DuckietownToken(version, payload, signature)
+
+    @classmethod
+    def generate(cls, key: SigningKey, user_id: int, days: int = 365, hours: int = 0, minutes: int = 0,
+                 scope: ScopeList = None) -> 'DuckietownToken':
+        if scope is None:
+            scope = DEFAULT_SCOPE
+        # make sure the scope is valid
+        if not isinstance(scope, list):
+            raise ValueError("Argument 'scope' must be a list")
+        scope_parsed: List[Scope] = []
+        scope_encoded: List[Union[str, dict]] = []
+        for s in scope:
+            if not isinstance(s, Scope):
+                s = Scope.parse(s)
+            scope_parsed.append(s)
+            scope_encoded.append(s.compact())
+        # compute expiration date
+        exp = None
+        if (days + hours + minutes) > 0:
+            now = datetime.datetime.now()
+            delta = datetime.timedelta(days=days, hours=hours, minutes=minutes)
+            exp = (now + delta).strftime(DATETIME_FORMAT[DEFAULT_VERSION])
+        # form payload
+        payload = {"uid": user_id, "scope": scope_encoded, "exp": exp}
+
+        def entropy(numbytes):
+            e = b"duckietown is a place of relaxed introspection, and hub extends this place a lot"
+            return e[:numbytes]
+
+        payload_bytes = str.encode(json.dumps(payload, sort_keys=True))
+        signature = key.sign(payload_bytes, entropy=entropy)
+
+        payload["scope"] = scope_parsed
+        return DuckietownToken(DEFAULT_VERSION, payload, signature)
---
+++ src/dt_authentication_tests/tests_dt2.py
@@ -0,0 +1,145 @@
+import datetime
+import json
+import logging
+import tempfile
+from typing import List
+
+from dt_authentication import DuckietownToken, InvalidToken
+from dt_authentication.token import DEFAULT_SCOPE, Scope
+from dt_authentication.utils import get_id_from_token, get_or_create_key_pair
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+SAMPLE_TOKEN = "dt2-FifqUXc5FqUotojkAVpYG5JWebZRH6SUHM3DRgk1kRSKdq1uKcL9CFaNT9dRhCVKwFoEnMgm6Hu6v8-" \
+               "43dzqWFnWd8KBa1yev1g3UKnzVxZkkTbfaF8L9aHLLmc5bPTTmQhQzybqT8rKAADZa"
+SAMPLE_TOKEN_UID = -1
+SAMPLE_TOKEN_EXP = "2024-05-06/02:48"
+
+
+def scope(s) -> str:
+    return json.dumps([str(s1) for s1 in s])
+
+
+def date(d) -> datetime.date:
+    return datetime.datetime.strptime(d, "%Y-%m-%d/%H:%M")
+
+
+def test1():
+    token = DuckietownToken.from_string(SAMPLE_TOKEN)
+    assert token.version == "dt2"
+    data = token.payload
+
+    assert data["uid"] == token.uid
+    assert date(data["exp"]) == token.expiration
+
+    assert token.uid == SAMPLE_TOKEN_UID
+
+    assert token.expiration == date(SAMPLE_TOKEN_EXP)
+    assert scope(token.scope) == scope(DEFAULT_SCOPE)
+
+    seq = SAMPLE_TOKEN[6:8]
+    msg_bad = SAMPLE_TOKEN.replace(seq, "XY")
+    try:
+        DuckietownToken.from_string(msg_bad)
+    except InvalidToken:
+        pass
+    else:
+        raise Exception(token)
+
+    assert SAMPLE_TOKEN_UID == get_id_from_token(SAMPLE_TOKEN)
+
+    try:
+        get_id_from_token(msg_bad)
+    except InvalidToken:
+        pass
+    else:
+        raise Exception()
+
+
+def test_verify1():
+    # in this token, uid is string
+    invalid = "dt2-wEJsmra4x9TT5bxVq1QYEEGQQW18bje1MvVmg4sDEdUSQjA4k-" \
+              "43dzqWFnWd8KBa1yev1g3UKnzVxZkkTbfTxZ1tQ5rvJk3Vaa9gHKwhoV38ms4nNR2X"
+    try:
+        DuckietownToken.from_string(invalid)
+    except InvalidToken:
+        pass
+    else:
+        msg = "Expected invalid"
+        raise Exception(msg)
+
+
+def test_create_simple():
+    with tempfile.TemporaryDirectory() as tmp:
+        sk, vk = get_or_create_key_pair("dt2", tmp)
+        token = DuckietownToken.generate(sk, SAMPLE_TOKEN_UID)
+        token.from_string(token.as_string(), vk=vk)
+        assert not token.expired
+
+
+def test_create_never_expires():
+    days: int = -1
+    with tempfile.TemporaryDirectory() as tmp:
+        sk, vk = get_or_create_key_pair("dt2", tmp)
+        token = DuckietownToken.generate(sk, SAMPLE_TOKEN_UID, days=days)
+        assert token.expiration is None
+        assert not token.expired
+
+
+def test_already_expired():
+    s: str = "dt2-FifqUXc5FqUotojkAUPJcyTpMcCh8x8xonB8Dc6PLo3VZcQGUcukHiYBzTjHAzkDEF9xApqFvvLc8L-" \
+             "43dzqWFnWd8KBa1yev1g3UKnzVxZkkTbfkRuAQ3kSBB4pz9LGE8epBAfwwttWthQ9b"
+    token = DuckietownToken.from_string(s)
+    assert token.expired
+
+
+def test_scope_action():
+    s: List[Scope] = [Scope("auth")]
+    with tempfile.TemporaryDirectory() as tmp:
+        sk, vk = get_or_create_key_pair("dt2", tmp)
+        token = DuckietownToken.generate(sk, SAMPLE_TOKEN_UID, scope=s)
+        assert scope(token.scope) == scope(s)
+        assert token.grants(s[0].action)
+
+
+def test_scope_resource():
+    s: List[Scope] = [Scope("create", "class")]
+    with tempfile.TemporaryDirectory() as tmp:
+        sk, vk = get_or_create_key_pair("dt2", tmp)
+        token = DuckietownToken.generate(sk, SAMPLE_TOKEN_UID, scope=s)
+        assert scope(token.scope) == scope(s)
+        assert token.grants(s[0].action, s[0].resource)
+
+
+def test_scope_resource_id():
+    s: List[Scope] = [Scope("create", "class", "55")]
+    with tempfile.TemporaryDirectory() as tmp:
+        sk, vk = get_or_create_key_pair("dt2", tmp)
+        token = DuckietownToken.generate(sk, SAMPLE_TOKEN_UID, scope=s)
+        assert scope(token.scope) == scope(s)
+        assert token.grants(s[0].action, s[0].resource, s[0].identifier)
+
+
+def test_scope_service():
+    s: List[Scope] = [Scope("create", "class", "55", "hub.duckietown.com")]
+    with tempfile.TemporaryDirectory() as tmp:
+        sk, vk = get_or_create_key_pair("dt2", tmp)
+        token = DuckietownToken.generate(sk, SAMPLE_TOKEN_UID, scope=s)
+        assert scope(token.scope) == scope(s)
+        assert token.grants(s[0].action, s[0].resource, s[0].identifier, s[0].service)
+
+
+def test_scope_not_granted():
+    s: List[Scope] = [Scope("create", "class")]
+    with tempfile.TemporaryDirectory() as tmp:
+        sk, vk = get_or_create_key_pair("dt2", tmp)
+        token = DuckietownToken.generate(sk, SAMPLE_TOKEN_UID, scope=s)
+        try:
+            # this is a less restricted scope than the one assigned
+            assert token.grants("create")
+        except AssertionError:
+            pass
+        else:
+            raise AssertionError("Expected to raise")
